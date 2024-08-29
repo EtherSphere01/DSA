b51bb3a3f76dc3796fc36978c04e166c16baeb45
Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2024-08-29 12:42:01 +0600

    update

--- Formulas/Access the same positon element from all side.cpp
+++ Formulas/Access the same positon element from all side.cpp
@@ -8,25 +8,27 @@ void accessFromAllSide(vector<vector<ll>>&arr){
     // Row = Layers;
     // Column = i;
 
-    ll n = arr.size()-1;
+   ll n = arr.size()-1;
     ll span = n-2;
     for (ll layer = 1; layer <= n / 2; layer++, span-=2){
         for (ll i = 0; i <= max(1LL*0,span); i++){
             cout << arr[layer][layer+i]<< " ";
-            cout << arr[layer][n-layer+1]<< " ";
-            cout << arr[n-layer+1][n-layer-i+1]<< " ";
-            cout << arr[n-layer+1][layer]<< " ";
+            cout << arr[layer+i][n-layer+1]<< " ";
+            cout << arr[n-layer+1][n-layer+1-i]<< " ";
+            cout << arr[n-layer+1-i][layer]<< " ";
              cout << endl;
         }
     }
 }
 
 int main(){
-    vector<vector<ll>>arr = {{0,0,0,0,0,0},
-                             {0,1,2,3,4,0},
-                             {0,5,6,7,8,0},
-                             {0,9,10,11,12,0},
-                             {0,13,14,15,16,0},
-                             {0,0,0,0,0,0}};
+    ll n;
+    cin>>n;
+    vector<vector<ll>>arr(n+1,vector<ll>(n+1,0));
+    for (ll i = 1; i <= n; i++){
+        for (ll j = 1; j <= n; j++){
+            cin>>arr[i][j];
+        }
+    }
     accessFromAllSide(arr);
 }
